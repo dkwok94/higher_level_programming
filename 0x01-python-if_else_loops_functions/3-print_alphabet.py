877c636a0008bdafbb15b01849eb72818af1e9f1
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-10 21:22:06 +0000

    Change string format

--- 0x01-python-if_else_loops_functions/3-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabet.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for letter in range(ord('a'),ord('z')):
-    if letter == ord('e') or letter == ord('q'):
+for letter in range(97, 123):
+    if letter == 101 or letter == 123:
         continue
-    print(chr(letter), end='')
+    print("{:c}".format(letter), end='')
