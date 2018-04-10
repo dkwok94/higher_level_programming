dc1965cb443e229309f324415c64443836c60bfd
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-10 21:34:42 +0000

    Add leading 0's in front of single digit numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0,100):
+for number in range(0, 100):
     if number == 99:
-        print("{:d}".format(number), end='\n')
+        print("{:02d}".format(number), end='\n')
     else:
-        print("{:d}".format(number), end=', ')
+        print("{:02d}".format(number), end=', ')
