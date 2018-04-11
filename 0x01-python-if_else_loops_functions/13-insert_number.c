ea248c37aae3b8a5ae41e949631e0256ca62af5f
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-11 03:02:06 +0000

    Change head NULL check edge case

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -20,6 +20,12 @@ listint_t *insert_node(listint_t **head, int number)
 	if (new == NULL)
 		return (NULL);
 	new->n = number;
+	if (*head == NULL)
+	{
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 	while (bottom != NULL)
 	{
 		if (bottom->n > new->n && top->n < new->n)
@@ -28,7 +34,7 @@ listint_t *insert_node(listint_t **head, int number)
 			top->next = new;
 			return (new);
 		}
-		else if (*head == NULL || (*head)->n > new->n)
+		else if ((*head)->n > new->n)
 		{
 			new->next = (*head);
 			(*head) = new;
