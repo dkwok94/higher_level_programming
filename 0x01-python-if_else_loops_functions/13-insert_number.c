d01ec22fab00ba5d1a8844b402d0f1a95783d44c
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-11 02:38:17 +0000

    Add function that inserts a node into a sorted linked list

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ *insert_node - inserts a node into a sorted list
+ *@head: the head of the linked list
+ *@number: the number data of the new node
+ *
+ *Return: the address of the new node
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+	listint_t *top;
+	listint_t *bottom;
+
+	top = *head;
+	bottom = (*head)->next;
+	if (head == NULL)
+		return (NULL);
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+	while (bottom != NULL)
+	{
+		if (bottom->n > new->n && top->n < new->n)
+		{
+			new->next = bottom;
+			top->next = new;
+			return (new);
+		}
+		else if ((*head)->n > new->n)
+		{
+			new->next = (*head);
+			(*head) = new;
+			return (new);
+		}
+		else
+		{
+			top = top->next;
+			bottom = bottom->next;
+		}
+	}
+	top->next = new;
+	new->next = NULL;
+	return (new);
+}
