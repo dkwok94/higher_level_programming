f8350173e95fe07c24cea6df58b384110b7e1bd1
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-04-19 07:00:57 +0000

    Pass in head double pointer instead of head single pointer

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -9,14 +9,14 @@
  * Return: 1 if a palindrome, 0 if not a palindrome
  */
 
-int palindrome_rec(listint_t *head, listint_t *tail)
+int palindrome_rec(listint_t **head, listint_t *tail)
 {
 	if (tail == NULL)
 		return (1);
 
-	if (palindrome_rec(head, tail->next) == 1 && head->n == tail->n)
+	if (palindrome_rec(head, tail->next) == 1 && (*head)->n == tail->n)
 	{
-		head = head->next;
+		(*head) = (*head)->next;
 		return (1);
 	}
 
@@ -36,8 +36,9 @@ int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
 		return (1);
+
 	if ((*head)->next == NULL)
 		return (1);
 
-	return (palindrome_rec(*head, *head));
+	return (palindrome_rec(head, *head));
 }
