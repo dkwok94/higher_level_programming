e63c2a4a5bb5a5560b4c19b7c1899478c0140105
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-05-08 01:06:56 +0000

    If one of the inputs is 0, perimeter is 0

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -92,5 +92,6 @@ class Rectangle:
 
     def perimeter(self):
         """Returns the perimeter of the instance"""
-
+        if self.__height == 0 or self.__width == 0:
+            return 0
         return 2 * self.__height + 2 * self.__width
