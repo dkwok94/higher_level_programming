88a59ea8695600ea31825bb9d62bb40742ee206c
dkwok94 <30913137+dkwok94@users.noreply.github.com>
2018-05-17 05:34:27 +0000

    Prevent code from running on import

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -3,13 +3,14 @@ from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
 
-try:
-    json_list = load_from_json_file("add_item.json")
-    i = 1
-    while (i < len(argv)):
-        json_list.append(argv[i])
-        i += 1
-    save_to_json_file(json_list, "add_item.json")
-except Exception as e:
-    json_list = []
-    save_to_json_file(json_list, "add_item.json")
+if __name__ == "__main__":
+    try:
+        json_list = load_from_json_file("add_item.json")
+        i = 1
+        while (i < len(argv)):
+            json_list.append(argv[i])
+            i += 1
+        save_to_json_file(json_list, "add_item.json")
+    except Exception as e:
+        json_list = []
+        save_to_json_file(json_list, "add_item.json")
