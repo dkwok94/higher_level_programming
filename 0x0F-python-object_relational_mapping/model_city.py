90590e9ad22fad2893f92909d5260b8c83f4c649
Derek <dhklangs@gmail.com>
2018-07-20 22:54:38 +0000

    Fix PEP8

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,6 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 from sqlalchemy.orm import relationship
 
+
 class City(Base):
     """
     City class
@@ -19,4 +20,3 @@ class City(Base):
                 nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(State.id), nullable=False)
-
