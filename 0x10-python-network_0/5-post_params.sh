2e5b3fee7f07fe8bc9a9540778e6f58b2227dce0
Derek <dhklangs@gmail.com>
2018-08-01 00:50:17 +0000

    Add com after email

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to the URL with certain variables
-curl $1 -s -X POST -d "email=hr%40holbertonschool%2E&subject=I%20will%20always%20be%20here%20for%20PLD"
+curl $1 -s -X POST -d "email=hr%40holbertonschool%2Ecom&subject=I+will+always+be+here+for+PLD"
