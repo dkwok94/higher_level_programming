ecaed30d42bf1cbaab9bb409de3aa8624479e18e
Derek <dhklangs@gmail.com>
2018-08-01 04:15:52 +0000

    Fix PEP8 issues

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,6 +3,7 @@
     Finds a peak in a list of unsorted integers
 """
 
+
 def find_peak(list_of_integers):
     """
         Finds a peak in a list of unsorted integers
@@ -20,13 +21,13 @@ def find_peak(list_of_integers):
 
     # Main recursion
 
-    mid = len(list_of_integers)//2
-    if list_of_integers[mid] >= list_of_integers[mid-1] and list_of_integers[mid] >= list_of_integers[mid+1]:
+    mid = len(list_of_integers) // 2
+    if list_of_integers[mid] >= list_of_integers[mid - 1] \
+            and list_of_integers[mid] >= list_of_integers[mid + 1]:
         return list_of_integers[mid]
 
-    if list_of_integers[mid+1] > list_of_integers[mid]:
-        return(find_peak(list_of_integers[mid+1:len(list_of_integers)]))
+    if list_of_integers[mid + 1] > list_of_integers[mid]:
+        return(find_peak(list_of_integers[mid + 1:len(list_of_integers)]))
 
-    if list_of_integers[mid-1] > list_of_integers[mid]:
+    if list_of_integers[mid - 1] > list_of_integers[mid]:
         return(find_peak(list_of_integers[0:mid]))
-     
