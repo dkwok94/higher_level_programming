20951bce5ab7315cfeabbda46a5270796ad89841
Derek <dhklangs@gmail.com>
2018-08-01 02:50:17 +0000

    Add -s option to curl

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a POST request to a URL with the contents of a JSON file
-curl $1 -X POST -H "Content-Type: application/json" -d @$2
+curl $1 -sX POST -H "Content-Type: application/json" -d @$2
