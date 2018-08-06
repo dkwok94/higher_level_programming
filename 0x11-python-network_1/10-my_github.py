1edbdc4b03d7ec7b7c2af69c9247dc82e1d66890
Derek <dhklangs@gmail.com>
2018-08-06 23:56:37 +0000

    Fix PEP8

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -13,6 +13,6 @@ if __name__ == '__main__':
     r = requests.get('https://api.github.com/user', auth=(username, password))
     if r.status_code != 200:
         print("None")
-    else:         
+    else:
         json_dict = r.json()
         print(json_dict['id'])
