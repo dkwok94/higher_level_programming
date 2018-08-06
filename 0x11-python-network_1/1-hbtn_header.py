fcbbea0a169d23171b48b30ec03cb394ccc48194
Derek <dhklangs@gmail.com>
2018-08-06 21:52:52 +0000

    Add task 1 which displays the Request ID of the request

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Takes a URL, sends a request, and displays the X-Request-Id
+"""
+
+import urllib.request
+from sys import argv
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(argv[1]) as response:
+        headers = dict(response.info())
+        print(headers['X-Request-Id'])
