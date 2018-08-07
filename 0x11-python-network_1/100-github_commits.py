f99d6aee7826cd2df868ca4e0a42b123cacd31c4
Derek <dhklangs@gmail.com>
2018-08-07 00:17:22 +0000

    Add task 100 which displays the first 10 commits on the rails repository

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Displays 10 commits of the repository "rails" by the user "rail"
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    repo = argv[1]
+    owner = argv[1]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(owner, repo)
+
+    r = requests.get(url)
+    commits = r.json()
+
+    for i in range(11):
+        commit = (commits[i])['commit']
+        name = (commit['author'])['name']
+        sha = (commit['tree'])['sha']
+        print("{}: {}".format(sha, name))
