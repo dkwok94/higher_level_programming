311b5baf1602305d3a0ab6f087120cf66febd55b
Derek <dhklangs@gmail.com>
2018-08-07 00:52:08 +0000

    Add task 101 which enables pagination when searching the Star Wars People API

---
+++ 0x11-python-network_1/101-starwars.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Deals with pagination on the Star Wars People API
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    search = argv[1]
+
+    url = url_first = 'https://swapi.co/api/people/'
+    
+
+    while url is not None:
+        r = requests.get(url, params={'search': search})
+        json = r.json()
+        if url == url_first:
+            print("Number of results: {}".format(json['count']))
+        for person in json['results']:
+            print(person['name'])
+        url = json['next']
