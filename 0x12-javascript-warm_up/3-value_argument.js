6321c2c2ab9c972db3f048d4ad65a2478040e62c
Derek <dhklangs@gmail.com>
2018-08-28 23:35:52 +0000

    h and checks if the value is undefined

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 // Prints the first argument passed to it
 
 const args = process.argv;
-if (args.length === 2) {
+if (args[2] === undefined) {
   console.log('No argument');
 } else {
   console.log(args[2]);
