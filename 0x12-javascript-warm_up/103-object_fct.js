0e26e0d816eb7d495da82f65778dae4c0120ba8d
Derek <dhklangs@gmail.com>
2018-08-29 02:09:21 +0000

    Fix styling to conform with semistandard style

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -4,9 +4,9 @@ let myObject = {
   value: 12
 };
 console.log(myObject);
-myObject.incr = function(){
+myObject.incr = function () {
   myObject.value += 1;
-}
+};
 myObject.incr();
 console.log(myObject);
 myObject.incr();
