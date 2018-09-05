c72611ccd2b7866f74971d396a2c1f5d5beb7f76
Derek <dhklangs@gmail.com>
2018-09-05 22:21:10 +0000

    Fix semistandard styling conformation

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -13,7 +13,7 @@ request(url, function (err, res, body) {
     for (let i = 0; i < films.length; i++) {
       for (let j = 0; j < films[i].characters.length; j++) {
         if (films[i].characters[j].includes('/18/')) {
-	  count++;
+          count++;
         }
       }
     }
